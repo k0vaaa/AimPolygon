1230a574f2df5f67a3079a39ca2a6012f40476ba
k0vaaa <kovalikalex03@mail.ru>
2022-11-24 16:55:41 +0300

    shooting mechanics fix
    added particle system, crosshair and damage to enemy

--- MovementTester/Assets/Scripts/Shooting.cs
+++ MovementTester/Assets/Scripts/Shooting.cs
@@ -4,21 +4,39 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    public float damage = 10f;
     public float range = 100f;
     public Camera cam;
+    public ParticleSystem particle;
    
     void Update()
     {   
         if (Input.GetButtonDown("Fire1")){
             Shoot();
+
         }
     }
     void Shoot()
-    {
+    {   
+        particle.Play();
         RaycastHit hit;
         if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range)){
-            Debug.DrawRay(cam.transform.position, cam.transform.forward*100f, Color.red,1,false);
+            // Debug.DrawRay(cam.transform.position, cam.transform.forward*100f, Color.red,1,false);
             Debug.Log(hit.transform.name);
+            
+            Target target = hit.transform.GetComponent<Target>();
+            
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            //     if(hit.transform.name =="EnemyBody"){
+            //         target.TakeDamage(damage);
+            // }
+            //     if(hit.transform.name =="EnemyHead"){
+            //         target.TakeDamage(damage*2);
+            // }
+
+            }
         }
     }
 }
