9c17d566970e4d05fdd07fd01b7667e7575c8069
Kovalik Alexander <67467547+k0vaaa@users.noreply.github.com>
2025-03-19 00:01:06 +0300

    Commit through the times
    updating game files for the project

--- MovementTester/Assets/Scripts/PlayerMovement.cs
+++ MovementTester/Assets/Scripts/PlayerMovement.cs
@@ -72,10 +72,10 @@ public class PlayerMovement : MonoBehaviour
         SpeedControl();
         StateHandler();
         if(grounded){
-            rb.drag = groundDrag;
+            rb.linearDamping = groundDrag;
         }
         else{
-            rb.drag = 0;
+            rb.linearDamping = 0;
         }
     }
 
@@ -143,11 +143,11 @@ public class PlayerMovement : MonoBehaviour
 
     private void SpeedControl() 
     {
-        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
         if (flatVel.magnitude > moveSpeed)
         {
             Vector3 limitedVel = flatVel.normalized * moveSpeed;
-            rb.velocity = new Vector3(limitedVel.x , rb.velocity.y, limitedVel.z);
+            rb.linearVelocity = new Vector3(limitedVel.x , rb.linearVelocity.y, limitedVel.z);
         }
         
         
@@ -156,7 +156,7 @@ public class PlayerMovement : MonoBehaviour
     
     private void Jump()
     {
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
      
